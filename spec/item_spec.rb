8be7d60007f496954aea98b1662bfa7845d3000b
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-03-07 11:04:27 -0500

    add item initialize tests

---
+++ spec/item_spec.rb
@@ -0,0 +1,15 @@
+require './lib/item'
+
+RSpec.describe Item do
+  before(:each) do
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      expect(@item2).to be_a(Item)
+      expect(item2.name).to eq('Tomato')
+      expect(@item2.price).to eq(0.5)
+    end
+  end
+end
