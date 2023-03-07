844323b9ddb73282ff52dd773f86016288f99851
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-03-07 11:38:59 -0500

    add potential_revenue test to vendor class

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -5,30 +5,48 @@ RSpec.describe Vendor do
   before(:each) do
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
-    @vendor = Vendor.new('Rocky Mountain Fresh')
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @vendor1 = Vendor.new('Rocky Mountain Fresh')
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack") 
   end
   describe '#initialize' do
     it 'exists and has attributes' do
-      expect(@vendor).to be_a(Vendor)
-      expect(@vendor.name).to eq('Rocky Mountain Fresh')
-      expect(@vendor.inventory).to eq({})
+      expect(@vendor1).to be_a(Vendor)
+      expect(@vendor1.name).to eq('Rocky Mountain Fresh')
+      expect(@vendor1.inventory).to eq({})
     end
   end
   describe '#check_stock' do
     it 'can check stock of certain items' do
 
-      expect(@vendor.check_stock(@item1)).to eq(0)
+      expect(@vendor1.check_stock(@item1)).to eq(0)
     end
 
     it 'can add stock for certain item' do
-      @vendor.stock(@item1, 30)
+      @vendor1.stock(@item1, 30)
 
-      expect(@vendor.check_stock(@item1)).to eq(30)
+      expect(@vendor1.check_stock(@item1)).to eq(30)
 
-      @vendor.stock(@item1, 25)
+      @vendor1.stock(@item1, 25)
 
-      expect(@vendor.check_stock(@item1)).to eq(55)
-      expect(@vendor.inventory).to eq({@item1=>55})
+      expect(@vendor1.check_stock(@item1)).to eq(55)
+      expect(@vendor1.inventory).to eq({@item1=>55})
+    end
+  end
+
+  describe '#potential_revenue' do
+    it 'can sum potential revenue' do
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+
+    expect(@vendor1.potential_revenue).to eq(29.75)
+    expect(@vendor2.potential_revenue).to eq(345.00)
+    expect(@vendor3.potential_revenue).to eq(48.75)
     end
   end
 end
