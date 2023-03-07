8db0b626bab2158c74e74dbeae373f95350dae61
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-03-07 11:12:32 -0500

    add vendor initialize tests

---
+++ spec/vendor_spec.rb
@@ -0,0 +1,17 @@
+require './lib/item'
+require './lib/vendor'
+
+RSpec.describe Vendor do
+  before(:each) do
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @vendor = Vendor.new('Rocky Mountain Fresh')
+  end
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      expect(@vendor).to be_a(Vendor)
+      expect(@vendor.name).to eq('Rocky Mountain Fresh')
+      expect(@vendor.inventory).to eq({})
+    end
+  end
+end
